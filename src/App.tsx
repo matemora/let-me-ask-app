098b1b9de16a700f0793ebb3d9f26440ce5bdc33
Mateus Morais <mateus.batista010@gmail.com>
2021-06-21 22:18:39 -0300

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
 import { Home } from './pages/Home';
+import { NewRoom } from './pages/NewRoom';
+import { BrowserRouter, Route } from 'react-router-dom';
+
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Route exact path="/" component={Home} />
+      <Route path="/rooms/new" component={NewRoom} />
+    </BrowserRouter>
+    // <Home />
+    // <NewRoom />
   );
 }
 
