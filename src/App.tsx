13980b11219c22d4ccd18029c76be634ec08753c
Mateus Morais <mateus.batista010@gmail.com>
2021-06-22 15:35:06 -0300

    added effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import { createContext, useState } from 'react';
 import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
@@ -19,6 +20,26 @@ export const AuthContext = createContext({} as AuthContextType);
 
 function App() {
   const [user, setUser] = useState<User>();
+
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(user => {
+      if (user) {
+        const { displayName, photoURL, uid } = user;
+  
+        if (!displayName || !photoURL) {
+          throw new Error('Missing information from Auth.')
+        }
+  
+        setUser({
+          id: uid,
+          name: displayName,
+          avatar: photoURL,
+        })
+      }
+    });
+
+    return () => unsubscribe()
+  }, [])
 
   async function signInWithGoogle() {
     const provider = new firebase.auth.GoogleAuthProvider();
