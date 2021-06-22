7f794c267df2d6839b0a8c0f798759b619f833d0
Mateus Morais <mateus.batista010@gmail.com>
2021-06-21 21:26:57 -0300

    added home

---
+++ src/pages/Home.tsx
@@ -0,0 +1,37 @@
+
+import illustrationImg from '../assets/images/illustration.svg';
+import logoImgSvg from '../assets/images/logo.svg';
+import googleIconImg from '../assets/images/google-icon.svg';
+
+import '../styles/auth.scss';
+
+export function Home() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="ilustracao_perguntas_respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas da sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div>
+          <img src={logoImgSvg} alt="logo_letmeask" />
+          <button>
+            <img src={googleIconImg} alt="logo_google" />
+            Crie sua sala com o google
+          </button>
+          <div>ou entre em uma sala</div>
+          <form>
+            <input
+              type="text"
+              placeholder="Digite o código da sala"
+            />
+            <button type="submit">
+              Entrar na sala
+            </button>
+          </form>
+        </div>
+      </main>
+    </div>
+  )
+}
