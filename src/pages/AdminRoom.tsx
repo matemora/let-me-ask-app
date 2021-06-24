353a38b7d2a3eecfce3fef262614aba3dfd0bda5
Mateus Morais <mateus.batista010@gmail.com>
2021-06-24 13:53:23 -0300

    added admin room page

---
+++ src/pages/AdminRoom.tsx
@@ -0,0 +1,89 @@
+import { useState, FormEvent } from "react";
+import { useParams } from "react-router-dom";
+import { Button } from "../components/Button";
+import { Question } from "../components/Question";
+import { RoomCode } from "../components/RoomCode";
+import { useAuth } from "../hooks/useAuth";
+import { useRoom } from "../hooks/useRoom";
+import { database } from "../services/firebase";
+import logoImg from '../assets/images/logo.svg';
+
+type RoomParams = {
+  id: string;
+}
+
+export function AdminRoom() {
+  const { user } = useAuth();
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
+  const [newQuestion, setNewQuestion] = useState('');
+  const { questions, title } = useRoom(roomId);
+
+  async function handleSendQuestion(event: FormEvent) {
+    event.preventDefault();
+
+    if (newQuestion.trim() === '') {
+      return;
+    }
+
+    if (!user) {
+      throw new Error('You must be logged in!');
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighLighted: false,
+      isAnswered: false,
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(question);
+    setNewQuestion('');
+  }
+  return (
+    <div id="page-room">
+      <header>
+        <div className="content">
+          <img src={logoImg} alt="letmeask_logo" />
+          <RoomCode code={roomId} />
+        </div>
+      </header>
+      <main>
+        <div className="room-title">
+          <h1>Sala {title}</h1>
+          {questions.length > 0 && (<span>{questions.length} pergunta{questions.length > 1 ? 's' : ''}</span>)}
+        </div>
+        <form onSubmit={handleSendQuestion}>
+          <textarea
+            placeholder="Compartilhe sua pergunta!"
+            onChange={e => setNewQuestion(e.target.value)}
+            value={newQuestion}
+          />
+          <div className="form-footer">
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>Para enviar uma pergunta, <button>faça seu login</button>.</span>
+            )}
+            <Button type="submit" disabled={!user}>Enviar pergunta</Button>
+          </div>
+        </form>
+        <div className="question-list">
+          {questions.map(question => (
+            <Question
+              key={question.id}
+              author={question.author}
+              content={question.content}
+            />
+          ))}
+        </div>
+      </main>
+    </div>
+  )
+}
