bd729bc704ad6efc3468845f0cdcb8da5b513e77
Mateus Morais <mateus.batista010@gmail.com>
2021-06-26 23:56:49 -0300

    refactor header and added theme control

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,15 +1,26 @@
 import { FC } from "react";
-import logoImg from '../assets/images/logo.svg';
+import { ThemeControl } from "./ThemeControl";
+import { RoomCode } from "./RoomCode";
+import { Logo } from "./Logo";
 import '../styles/header.scss';
+import { useParams } from "react-router-dom";
+
+type RoomParams = {
+  id: string;
+}
 
 export const Header: FC = ({
   children
 }) => {
+  const params = useParams<RoomParams>();
+  const roomId = params.id;
   return (
     <header>
       <div className="content">
-        <img src={logoImg} alt="letmeask_logo" />
+        <Logo />
         <div className="room-controls">
+          <RoomCode code={roomId} />
+          <ThemeControl />
           {children}
         </div>
       </div>
