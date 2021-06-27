b6efe63cf2d692eb8fdc60ca070f574b8ad21ddc
Mateus Morais <mateus.batista010@gmail.com>
2021-06-27 00:43:40 -0300

    refactor logo

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,7 +1,7 @@
 import { FormEvent, useState } from 'react';
 import { Link, useHistory } from 'react-router-dom';
 import illustrationImg from '../assets/images/illustration.svg';
-import logoImgSvg from '../assets/images/logo.svg';
+import { Logo } from '../components/Logo';
 import { Button } from '../components/Button';
 import { useAuth } from '../hooks/useAuth';
 import { database } from '../services/firebase';
@@ -36,7 +36,7 @@ export function NewRoom() {
       </aside>
       <main>
         <div className="main-content">
-          <img src={logoImgSvg} alt="logo_letmeask" />
+          <Logo />
           <h2>Criar uma nova sala</h2>
           <form onSubmit={handleCreateRoom}>
             <input
@@ -50,7 +50,7 @@ export function NewRoom() {
             </Button>
           </form>
           <p>
-            Quer entrar em uma sala existente? <Link to="/">clique aqui</Link>
+            Quer entrar em uma sala existente? <Link to="/">Clique aqui</Link>
           </p>
         </div>
       </main>
